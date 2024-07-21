0dcca5fa92982efbea58f23b505bf1550f0831a1
Nagyeong3 <skrud0318@naver.com>
2024-07-21 23:27:17 +0900

    Update table, button style 변경

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,7 +1,7 @@
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 import styles from "../Styles/css/Table.module.css";
-function Table({ columns, rows }) {
+function Table({ columns, rows, onRowSelection }) {
   return (
     <div id={styles.table_body}>
       <DataGrid
@@ -15,12 +15,15 @@ function Table({ columns, rows }) {
         }}
         rows={rows}
         columns={columns}
+        onRowSelectionModelChange={(newSelection) =>
+          onRowSelection(newSelection)
+        }
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 10 },
           },
         }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[5, 10, 15]}
         checkboxSelection
       />
     </div>
