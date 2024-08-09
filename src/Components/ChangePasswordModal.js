6cc9ef8f241d9d929480dd0ab55bc2fcc30503f3
Nagyeong3 <skrud0318@naver.com>
2024-08-09 11:43:53 +0900

    Add change password page

---
+++ src/Components/ChangePasswordModal.js
@@ -0,0 +1,246 @@
+import React, { useState } from "react";
+import { styled, css } from "@mui/system";
+import { grey } from "../Styles/Color"; // 색상 팔레트 임포트
+import { useNavigate } from "react-router-dom";
+
+import {
+  Box,
+  TextField,
+  Grid,
+  IconButton,
+  InputAdornment,
+  Modal as BaseModal,
+  Backdrop,
+  Button,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+function CustomModal({ open, handleClose, title, rowData, rowsHeader }) {
+  const [password, setPassword] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [showCurrentPassword, setShowCurrentPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showRePassword, setShowRePassword] = useState(false);
+  const [rePasswordError, setRePasswordError] = useState(false);
+
+  const handleSave = () => {
+    console.log(JSON.stringify(rowData) + "changing to " + password);
+  };
+
+  const handleClickShowCurrentPassword = () => {
+    setShowCurrentPassword(!showCurrentPassword);
+  };
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleClickShowRePassword = () => {
+    setShowRePassword(!showRePassword);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    checkPasswords(event.target.value, rePassword);
+  };
+  const handleCurrentPasswordChange = (event) => {
+    setCurrentPassword(event.target.value);
+  };
+  const handleRePasswordChange = (event) => {
+    setRePassword(event.target.value);
+    checkPasswords(password, event.target.value);
+  };
+
+  const checkPasswords = (pw, rePw) => {
+    if (pw.length > 0 && rePw.length > 0 && pw !== rePw) {
+      setRePasswordError(true);
+    } else {
+      setRePasswordError(false);
+    }
+  };
+  return (
+    <div>
+      <Modal
+        aria-labelledby="keep-mounted-modal-title"
+        aria-describedby="keep-mounted-modal-description"
+        open={open}
+        onClose={handleClose}
+        slots={{ backdrop: StyledBackdrop }}
+        keepMounted
+      >
+        <ModalContent
+          sx={{
+            width: 400,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <h2 id="keep-mounted-modal-title" className="modal-title">
+            {title}
+          </h2>
+          <Box sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}>
+            <Grid container spacing={2} sx={{ maxWidth: 600 }}>
+              <Grid item xs={12} lg={12}>
+                <TextField
+                  fullWidth
+                  label="Current PW"
+                  variant="outlined"
+                  type={showCurrentPassword ? "text" : "password"}
+                  value={currentPassword}
+                  onChange={handleCurrentPasswordChange}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowCurrentPassword}
+                          edge="end"
+                        >
+                          {showCurrentPassword ? (
+                            <VisibilityOff />
+                          ) : (
+                            <Visibility />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} lg={12}>
+                <TextField
+                  fullWidth
+                  label="PW"
+                  variant="outlined"
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  onChange={handlePasswordChange}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} lg={12}>
+                <TextField
+                  fullWidth
+                  label="Re-type PW"
+                  variant="outlined"
+                  type={showRePassword ? "text" : "password"}
+                  value={rePassword}
+                  onChange={handleRePasswordChange}
+                  error={rePasswordError}
+                  helperText={rePasswordError ? "Passwords do not match" : ""}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowRePassword}
+                          edge="end"
+                        >
+                          {showRePassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                sx={{ display: "flex", justifyContent: "flex-end" }}
+              >
+                <Button
+                  variant="outlined"
+                  color="error"
+                  onClick={handleClose}
+                  sx={{ marginRight: 2 }}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleSave}
+                >
+                  Save
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+}
+
+const Modal = styled(BaseModal)(`
+  position: fixed;
+  z-index: 1300;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &.base-Modal-hidden {
+    visibility: hidden;
+  }
+`);
+
+const StyledBackdrop = styled(Backdrop)`
+  z-index: -1;
+  position: fixed;
+  inset: 0;
+  background-color: rgb(0 0 0 / 0.5);
+  -webkit-tap-highlight-color: transparent;
+`;
+
+const ModalContent = styled("div")(
+  ({ theme }) => css`
+    height: 70vh;
+    font-family: "IBM Plex Sans", sans-serif;
+    font-weight: 500;
+    text-align: start;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    overflow: hidden;
+    background-color: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+    border-radius: 8px;
+    border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+    box-shadow: 0 4px 12px
+      ${theme.palette.mode === "dark" ? "rgb(0 0 0 / 0.5)" : "rgb(0 0 0 / 0.2)"};
+    padding: 24px;
+    color: ${theme.palette.mode === "dark" ? grey[50] : grey[900]};
+
+    & .modal-title {
+      margin: 0;
+      line-height: 1.5rem;
+      margin-bottom: 8px;
+    }
+
+    & .modal-description {
+      margin: 0;
+      line-height: 1.5rem;
+      font-weight: 400;
+      color: ${theme.palette.mode === "dark" ? grey[400] : grey[800]};
+      margin-bottom: 4px;
+    }
+  `
+);
+
+export default CustomModal;
---
+++ src/Pages/ChangePassword.js
@@ -0,0 +1,143 @@
+import React, { useState, useNavigate } from "react";
+
+import AppBar from "../Components/AppBar";
+import Table from "../Components/Table";
+import Button from "../Components/Button";
+import styles from "../Styles/css/Table.module.css";
+import Modal from "../Components/ChangePasswordModal";
+
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+import Radio from "@mui/material/Radio";
+import { Typography, Box } from "@mui/material";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+const columns = [
+  {
+    field: "sn",
+    headerName: "SN",
+    flex: 0.05,
+    cellClassName: styles.centerAlign,
+  },
+  { field: "name", headerName: "Name", flex: 0.2 },
+  { field: "gender", headerName: "Gender", flex: 0.1 },
+  { field: "birth", headerName: "Birth", flex: 0.1 },
+  { field: "id", headerName: "ID", flex: 0.2 },
+  { field: "grade", headerName: "Grade", flex: 0.3 },
+];
+
+function createData(sn, gender, name, birth, id, grade) {
+  return { sn, gender, name, birth, id, grade };
+}
+
+const rows = [
+  createData(1, "Male", "Jon", "20.02.24", "a0000", "PlayGroup"),
+  createData(2, "Female", "Cersei", "20.01.04", "a0001", "PlayGroup"),
+  createData(3, "Male", "Jaime", "20.12.24", "a0002", "PlayGroup"),
+  createData(4, "Male", "Arya", "20.05.27", "a0003", "PlayGroup"),
+  createData(5, "Male", "Daenerys", "20.08.14", "a0004", "PlayGroup"),
+  createData(6, "Male", "nell", "20.12.24", "a0005", "PlayGroup"),
+  createData(7, "Female", "Ferrara", "19.07.05", "b0006", "UnderKG"),
+  createData(8, "Female", "Rossini", "19.07.25", "b0007", "UnderKG"),
+  createData(9, "Female", "Harvey", "19.07.04", "b0008", "UnderKG"),
+];
+
+function ChangePassword() {
+  // const [selectedRows, setSelectedRows] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalRowData, setModalRowData] = useState("default row data");
+  const [alert, setAlert] = useState(false);
+  const [checkedRow, setCheckedRow] = useState(null); // 단일 값으로 변경
+  const handleRowSelection = (id) => {
+    setCheckedRow(id);
+  };
+  const handleModalOpen = (row) => {
+    setModalRowData(row);
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+    setModalRowData(null);
+  };
+  const goChangePasswordView = (rowData) => {
+    handleModalOpen(rowData);
+  };
+  const updatedColumns = [
+    {
+      field: "check",
+      headerName: "",
+      flex: 0.05,
+      renderCell: (params) => (
+        <Radio
+          {...label}
+          checked={checkedRow === params.row.id}
+          onChange={() => handleRowSelection(params.row.id)}
+        />
+      ),
+    },
+    ...columns,
+  ];
+
+  return (
+    <div id="page_content">
+      <AppBar />
+      {alert ? (
+        <Stack
+          sx={{ width: "100%", position: "fixed", top: "65px" }}
+          spacing={2}
+        >
+          <Alert severity="success">This is a success Alert.</Alert>
+        </Stack>
+      ) : (
+        <div></div>
+      )}
+
+      <div id={styles.table_container}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: 0,
+            marginBottom: 3,
+          }}
+        >
+          <Typography
+            variant="h3"
+            component="div"
+            sx={{ fontFamily: "Copperplate" }}
+          >
+            Student Board
+          </Typography>
+        </Box>
+        <Table
+          columns={updatedColumns}
+          rows={rows}
+          onRowSelection={handleRowSelection}
+          onRowClick={(params) => handleRowSelection(params.row.id)}
+          onRowDoubleClick={(params) => handleModalOpen(params.row)}
+          getRowId={(row) => row.sn}
+          isRadioButton={true}
+        />
+      </div>
+      <Button
+        title={"Change"}
+        onClick={goChangePasswordView}
+        disabled={checkedRow == null}
+        id={"view_btn"}
+        size={"bg"}
+      />
+
+      <Modal
+        open={modalOpen}
+        handleClose={handleModalClose}
+        title={"Detail Information"}
+        rowData={modalRowData}
+        rowsHeader={columns}
+      />
+    </div>
+  );
+}
+
+export default ChangePassword;
