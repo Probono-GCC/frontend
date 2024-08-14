c09bc5ca4cc6f2de23d060282c84c3c2abfe55dc
yjwon <yjwon20@naver.com>
2024-08-14 17:00:01 +0900

    Axios Connect First Try

--- src/Apis/Api/User.js
+++ src/Apis/Api/User.js
@@ -1,19 +1,39 @@
 // src/Apis/Api/User.js
 import { axiosInstance } from "../Utils/Axios";
 
-export async function postTeacher() {
+export async function postTeacher(userData) {
   try {
-    const response = await axiosInstance.post(`/teachers`);
+    const response = await axiosInstance.post(`/teachers`, userData);
     return response.data;
   } catch (err) {
     console.log(err);
   }
 }
 
-export async function getTeachers(userData) {
+export async function getTeachers() {
   try {
     const response = await axiosInstance.get("/teachers");
     // console.log(response);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function postStudent(userData) {
+  try {
+    const response = await axiosInstance.post("/students", userData);
+    console.log(response);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getStudents() {
+  try {
+    const response = await axiosInstance.get("/students");
+    //console.log(response);
     return response.data;
   } catch (err) {
     console.log(err);
