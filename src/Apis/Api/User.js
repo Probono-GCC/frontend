f832672ab6f802342ba9d9e93f9146e919872c7a
Nagyeong3 <skrud0318@naver.com>
2024-08-19 16:22:08 +0900

    Update User.js

--- src/Apis/Api/User.js
+++ src/Apis/Api/User.js
@@ -40,24 +40,68 @@ export async function getStudents() {
   }
 }
 
-
 export async function loginApi(userData) {
   try {
     const response = await axiosInstance.post(`/login`, userData);
     return response;
   } catch (err) {
+    // console.log(err);
+    return err;
+  }
+}
+export async function putStudent(userData, loginId) {
+  try {
+    const response = await axiosInstance.put(`/students/${loginId}`, userData);
+    console.log(response);
+    return response;
+  } catch (err) {
     console.log(err);
   }
 }
-export async function putStudent(userData) {
+export async function postProfileImage(userData, userId) {
   try {
-    const response = await axiosInstance.put(
-      `/students/${userData.loginId}`,
-      userData
+    const response = await axiosInstance.post(
+      `/profile/images/${userId}`,
+      userData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data", // `FormData`를 전송할 때 자동으로 설정됩니다.
+        },
+      }
     );
-    console.log(response);
     return response.data;
-
+  } catch (err) {
+    console.log(err);
+  }
+}
+export async function getStudent(userId) {
+  try {
+    const response = await axiosInstance.get(`/students/${userId}`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+export async function getTeacher(userId) {
+  try {
+    const response = await axiosInstance.get(`/teachers/${userId}`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+export async function putTeacher(userData, loginId) {
+  try {
+    const response = await axiosInstance.put(`/teachers/${loginId}`, userData);
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
+}
+export async function getProfileImage(userId) {
+  try {
+    const response = await axiosInstance.get(`/profile/images/${userId}`);
+    return response.data;
   } catch (err) {
     console.log(err);
   }
