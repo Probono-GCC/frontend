7b443ae434e189de8ea70e14ec180be17028d95a
Nagyeong3 <skrud0318@naver.com>
2024-08-22 20:39:16 +0900

    Update Post.js

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -5,7 +5,6 @@ import AppBar from "../Components/AppBar";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import PersonIcon from "@mui/icons-material/Person";
 import { useMediaQueryContext } from "../store/MediaQueryContext";
-
 //api
 import { deleteNoticePost, getNoticePost } from "../Apis/Api/Notice";
 
@@ -41,7 +40,7 @@ function Post() {
     if (window.confirm("Do you want to delete the post?")) {
       deleteNoticePost(postData.noticeId).then(() => {
         window.alert("Deleted");
-        navigate("/notice-board");
+        navigate(-1);
       });
     }
   };
@@ -170,7 +169,7 @@ function Post() {
               marginBottom: 3,
               marginLeft: 3,
               marginRight: 3,
-              minHeight: "10vh",
+              minHeight: "60vh",
               overflowY: "scroll",
             }}
           >
@@ -199,12 +198,18 @@ function Post() {
             <Button
               variant="outlined"
               color="error"
-              sx={{ marginRight: 2 }}
+              sx={{ marginRight: 2, minHeight: "50px", minWidth: "120px" }}
               onClick={handleDelete}
             >
               Delete
             </Button>
-            <Button variant="contained" color="primary" onClick={handleEdit}>
+
+            <Button
+              variant="contained"
+              sx={{ marginRight: 2, minHeight: "50px", minWidth: "100px" }}
+              color="primary"
+              onClick={handleEdit}
+            >
               Edit
             </Button>
           </Box>
