a5b6a10c0cbf3f2a54dfa234556c59a4c68da0e6
Nagyeong3 <skrud0318@naver.com>
2024-08-29 21:51:25 +0900

    Update Text to speech 기능 추가

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -6,7 +6,7 @@ import VisibilityIcon from "@mui/icons-material/Visibility";
 import PersonIcon from "@mui/icons-material/Person";
 import { IconButton } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-
+import TextToSpeech from "../Util/TextToSpeech";
 import { convertDateFormat } from "../Util/DateUtils";
 
 import { useMediaQueryContext } from "../store/MediaQueryContext";
@@ -16,6 +16,7 @@ import { deleteNoticePost, getNoticePost } from "../Apis/Api/Notice";
 import { postTranslationData } from "../Apis/Api/Translate";
 
 import i18n from "../i18n/i18n";
+import { useTranslation } from "react-i18next";
 
 import { useAuth } from "../store/AuthContext";
 
@@ -25,6 +26,7 @@ function Post() {
   const navigate = useNavigate();
   const [tempImageList, setTempImageList] = useState([]);
   const [postedData, setPostedData] = useState([]); //서버에서 받아온 포스팅 데이터
+  const { t } = useTranslation();
 
   const [translatedContent, setTranslatedContent] = useState(""); //번역
   const [translatedTitle, setTranslatedTitle] = useState("");
@@ -38,7 +40,7 @@ function Post() {
   // 현재 URL에서 경로(path)를 가져옴
   const path = window.location.pathname;
 
-  const { userRole, roleArray, userData } = useAuth();
+  const { userRole } = useAuth();
 
   // 경로를 슬래시(/)로 분리하여 배열로 변환
   const pathSegments = path.split("/");
@@ -66,107 +68,72 @@ function Post() {
       });
     }
   };
-  const checkNepaliVoiceSupport = () => {
-    // 음성 목록을 가져오는 함수
-    const getVoices = () => {
-      return new Promise((resolve) => {
-        const voices = window.speechSynthesis.getVoices();
-        console.log("voice list?", voices);
-        if (voices.length) {
-          resolve(voices);
-        } else {
-          // 브라우저에서 음성 목록이 비어 있을 때, 음성 목록이 로드될 때까지 대기
-          window.speechSynthesis.onvoiceschanged = () => {
-            resolve(window.speechSynthesis.getVoices());
+
+  const fetchData = async () => {
+    // console.log("enter into fetchData");
+    try {
+      // 1. 먼저 공지사항 데이터를 가져옴
+      const result = await getNoticePost(postData.noticeId);
+      // console.log("get notice post", result);
+
+      // 2. 공지사항 데이터를 상태로 먼저 업데이트
+      if (result) {
+        setPostedData({
+          title: result.title,
+          content: result.content, // 초기에는 원본 content로 설정
+          updatedAt: result.updatedAt,
+          views: result.views,
+          createdChargeId: result.createdChargeId,
+        });
+      }
+
+      // 3. 번역 요청
+      if (result && result.content) {
+        try {
+          // 내용 번역
+          const contentTranslationData = {
+            text: result.content,
+            to: i18n.language,
           };
-        }
-      });
-    };
+          const translationContentResult = await PostTranslation(
+            contentTranslationData
+          );
+          console.log("Translation content result:", translationContentResult);
 
-    getVoices()
-      .then((voices) => {
-        // 네팔어 음성 찾기
-        const nepaliVoice = voices.find((voice) => voice.lang.startsWith("ne"));
+          // 번역 결과가 있으면 상태를 업데이트
+          setTranslatedContent(translationContentResult.translatedText);
 
-        if (nepaliVoice) {
-          console.log("Nepali voice is available.");
-        } else {
-          console.log("Nepali voice is not available.");
+          // 제목 번역
+          const titleTranslationData = {
+            text: result.title,
+            to: i18n.language,
+          };
+          const translationTitleResult = await PostTranslation(
+            titleTranslationData
+          );
+          console.log("Translation title result:", translationTitleResult);
+
+          // 번역 결과가 있으면 상태를 업데이트
+          setTranslatedTitle(translationTitleResult.translatedText);
+        } catch (error) {
+          console.error("Failed to translate:", error);
         }
-      })
-      .catch((error) => {
-        console.error("Error checking Nepali voice support:", error);
-      });
+      }
+
+      // 4. 이미지 리스트가 있으면 상태 업데이트
+      if (result && result.imageList != null) {
+        postData.imageList = result.imageList;
+        setTempImageList(result.imageList);
+      } else {
+        setTempImageList([]);
+        postData.imageList = null;
+      }
+    } catch (error) {
+      console.error("Error fetching notice post:", error);
+    }
   };
-
   useEffect(() => {
     console.log("i18n.language", i18n.language);
-    const fetchData = async () => {
-      console.log("enter into fetchData");
-      try {
-        // 1. 먼저 공지사항 데이터를 가져옴
-        const result = await getNoticePost(postData.noticeId);
-        console.log("get notice post", result);
-
-        // 2. 공지사항 데이터를 상태로 먼저 업데이트
-        if (result) {
-          setPostedData({
-            title: result.title,
-            content: result.content, // 초기에는 원본 content로 설정
-            updatedAt: result.updatedAt,
-            views: result.views,
-            createdChargeId: result.createdChargeId,
-          });
-        }
-
-        // 3. 번역 요청
-        if (result && result.content) {
-          try {
-            // 내용 번역
-            const contentTranslationData = {
-              text: result.content,
-              to: i18n.language,
-            };
-            const translationContentResult = await PostTranslation(
-              contentTranslationData
-            );
-            console.log(
-              "Translation content result:",
-              translationContentResult
-            );
-
-            // 번역 결과가 있으면 상태를 업데이트
-            setTranslatedContent(translationContentResult.translatedText);
-
-            // 제목 번역
-            const titleTranslationData = {
-              text: result.title,
-              to: i18n.language,
-            };
-            const translationTitleResult = await PostTranslation(
-              titleTranslationData
-            );
-            console.log("Translation title result:", translationTitleResult);
-
-            // 번역 결과가 있으면 상태를 업데이트
-            setTranslatedTitle(translationTitleResult.translatedText);
-          } catch (error) {
-            console.error("Failed to translate:", error);
-          }
-        }
-
-        // 4. 이미지 리스트가 있으면 상태 업데이트
-        if (result && result.imageList != null) {
-          postData.imageList = result.imageList;
-          setTempImageList(result.imageList);
-        } else {
-          setTempImageList([]);
-          postData.imageList = null;
-        }
-      } catch (error) {
-        console.error("Error fetching notice post:", error);
-      }
-    };
 
     fetchData(); // 데이터 불러오기 함수 호출
   }, [i18n.language, postData.noticeId]);
@@ -183,10 +150,11 @@ function Post() {
   // }, [translatedContent, translatedTitle]);
 
   const handleBack = () => {
-    navigate(-1); // Go back to the previous page
+    navigate(-1);
   };
+  // 데이터 로딩중일 때
   if (!postData) {
-    return <div>Loading...</div>; // 데이터를 로딩 중일 때 보여줄 내용
+    return <div>Loading...</div>;
   }
   if (!tempImageList) {
     return <div>Loading...</div>;
@@ -276,6 +244,7 @@ function Post() {
                 {convertDateFormat(postedData.updatedAt)}
               </Typography>
             </Grid>
+
             <Grid
               item
               xs={4}
@@ -304,33 +273,58 @@ function Post() {
               </Box>
             </Grid>
           </Grid>
-
-          <Typography
-            variant="body1"
+        </Paper>
+        <Paper
+          sx={{
+            paddingTop: 1,
+            paddingBottom: 2,
+            paddingLeft: 3,
+            paddingRight: 3,
+            position: "relative", // 내부 요소의 상대적 위치 설정
+          }}
+          elevation={0}
+        >
+          <Box
             sx={{
-              marginBottom: 3,
-              marginTop: 2,
-              minHeight: "20vh",
-              maxWidth: "100vw", // 컨텐츠가 부모 요소를 넘지 않도록 함
-              // overflowY: "scroll",
-              border: "1px solid #e0e0e0",
-              // borderTop: "1px solid #e0e0e0",
-              // borderBottom: "1px solid #e0e0e0",
-              borderRadius: "8px",
-              padding: 2,
-              wordBreak: "break-word", // 긴 단어를 줄 바꿈하여 보더 밖으로 넘치지 않도록 함
-              boxSizing: "border-box", // 보더와 패딩을 포함하여 요소의 전체 크기를 계산
+              position: "relative", //상대적 위치
             }}
-            dangerouslySetInnerHTML={{ __html: postedData.content }}
-          ></Typography>
+          >
+            <Typography
+              variant="body1"
+              sx={{
+                marginBottom: 3,
+                marginTop: 2,
+                minHeight: "20vh",
+                maxWidth: "100vw",
+                border: "1px solid #e0e0e0",
+                borderRadius: "8px",
+                padding: 2,
+                wordBreak: "break-word",
+                boxSizing: "border-box",
+                paddingBottom: "40px",
+              }}
+              dangerouslySetInnerHTML={{ __html: postedData.content }}
+            />
+            {/* 스피커 아이콘 */}
+            {/* <Box
+              sx={{
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+                padding: 2,
+              }}
+            >
+              <TextToSpeech text={postedData.content} />
+            </Box> */}
+          </Box>
 
           {tempImageList && tempImageList.length !== 0 ? (
             <Box
               sx={{
                 textAlign: "center",
                 marginBottom: 3,
-                maxWidth: "95%", // body1과 동일한 너비
-                margin: "0 auto", // 가운데 정렬
+                maxWidth: "95%",
+                margin: "0 auto",
               }}
             >
               {tempImageList.map((image, index) => (
@@ -341,7 +335,7 @@ function Post() {
                   style={{
                     maxWidth: "100%",
                     height: "auto",
-                    margin: "10px 0", // 이미지 간격 조정
+                    margin: "10px 0",
                   }}
                 />
               ))}
