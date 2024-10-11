2b6c4161c6f5b5938c5555ff8c44767914600540
Nagyeong3 <skrud0318@naver.com>
2024-10-11 17:10:28 +0900

    Fix 번역 key수정

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -9,11 +9,11 @@ i18n
   .use(initReactI18next) // react-i18next 바인딩
   .init({
     resources: {
-      eng: { translation: enTranslation },
-      nep: { translation: nepalTranslation },
+      en: { translation: enTranslation },
+      ne: { translation: nepalTranslation },
     },
-    lng: "eng", // 기본 언어를 영어로 설정
-    fallbackLng: "eng", // 지원하지 않는 언어가 있을 경우 기본 언어 설정
+    lng: "en", // 기본 언어를 영어로 설정
+    fallbackLng: "en", // 지원하지 않는 언어가 있을 경우 기본 언어 설정
     debug: false, // 디버깅 활성화
     interpolation: {
       escapeValue: false, // React는 XSS 방지가 기본
